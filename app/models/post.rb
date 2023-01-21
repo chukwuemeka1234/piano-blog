bade1b114cc9694065e4fa9a62b4ff4498ea67bc
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-01-21 23:23:07 +0100

    Add validate to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  has_many :posts, inverse_of: :recipe
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :instructions, presence: true, length: { maximum: 1000 }
+  validates :cooking_time, presence: true, numericality: { greater_than: 0 }
 end
